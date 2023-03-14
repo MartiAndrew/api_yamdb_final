751e754a7f163026ac1094434bfd0a21136ac4a8
Andrew_Mishkov <andrew.mishkov@gmail.com>
2023-03-15 00:40:38 +0300

    factoring: created TitleSerializer

--- api_yamdb/reviews/serializers.py
+++ api_yamdb/reviews/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
-from .models import Comment, Review
+from .models import Comment, Review, Category, Title
+
+class TitleSerializer(ModelSerializer):
+    category = SlugRelatedField(queryset=Category.objects.all(),
+                                slug_field='slug')
+    genre = SlugRelatedField(queryset=Genre.objects.all(),
+                             slug_field='slug')
+    class Meta:
+        fields = 'name', 'year', 'description', 'genre', 'category'
+        model = Title
 
 
 class ReviewSerializer(ModelSerializer):
