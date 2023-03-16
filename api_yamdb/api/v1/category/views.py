4f0e5b777c09c1f8a64a3bd4c8a68e897bb5893d
Andrew <andrew.mishkov@gmail.com>
2023-03-16 12:36:55 +0300

    feat: add CategoryViewSet

---
+++ api_yamdb/api/v1/category/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, filters, mixins
+from rest_framework.pagination import LimitOffsetPagination
+
+from reviews.models import Category
+from ..category.serializers import CategorySerializer
+from ..user.permissions import AdminPermission
+
+
+class CategoryViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (AdminPermission, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('category__slug',)
