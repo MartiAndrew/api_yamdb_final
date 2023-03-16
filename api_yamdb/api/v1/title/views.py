e08875f57fd1d6d7243e8e777d28fb031de10b7e
Andrew <andrew.mishkov@gmail.com>
2023-03-16 10:10:07 +0300

    refactor: rename files for Serializers, add permissions_classes and pagination_class for Title

--- api_yamdb/api/v1/title/views.py
+++ api_yamdb/api/v1/title/views.py
@@ -1,12 +1,15 @@
 from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
 
-from .models import Title
-from .serializers import TitleSerializer
+from reviews.models import Title
+from ..title.serializers import TitleSerializer
+from ..user.permissions import AdminPermission
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = TitleSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (AdminPermission, )
     queryset = Title.objects.all()
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'year', 'genre__slug', 'category__slug')
